c657a154d778e19d4005b822f99448ab271d78f2
eudevnane <nanewjmorais@icloud.com>
2023-10-04 12:30:40 -0300

    Projeto finalizado

---
+++ index.js
@@ -0,0 +1,41 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;   
+      this.idade = idade; 
+      this.tipo = tipo;   
+    }
+
+
+    atacar() {
+      let ataque = ""; 
+
+
+      switch (this.tipo) {
+        case "mago":
+          ataque = " magia"; 
+          break;
+        case "guerreiro":
+          ataque = " espada"; 
+          break;
+        case "monge":
+          ataque = " artes marciais"; 
+          break;
+        case "ninja":
+          ataque = " shuriken"; 
+          break;
+        default:
+          ataque = "não possui ataque definido"; 
+      }
+      
+
+      console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+
+  
+
+  const heroi1 = new Heroi("Herói 1", 28, "ninja"); 
+  const heroi2 = new Heroi("Herói 2", 35, "mago"); 
+
+  heroi1.atacar(); 
+  heroi2.atacar(); 
